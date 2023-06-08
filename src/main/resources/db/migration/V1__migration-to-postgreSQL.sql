bb23299c56a3259215c03bc2e03360a09f6e1898
JaelsonJonas <rm93915@fiap.com.br>
2023-06-08 19:33:00 -0300

    first commit

---
+++ src/main/resources/db/migration/V1__migration-to-postgreSQL.sql
@@ -0,0 +1,43 @@
+create table usuarios(
+
+    id bigserial not null primary key,
+    login varchar(100) not null,
+    senha varchar(255) not null
+
+);
+
+create table pacientes(
+
+    id bigserial not null primary key,
+    nome varchar(100) not null,
+    email varchar(100) not null unique,
+    cpf varchar(14) not null unique,
+    logradouro varchar(100) not null,
+    bairro varchar(100) not null,
+    cep varchar(9) not null,
+    complemento varchar(100),
+    numero varchar(20),
+    uf char(2) not null,
+    cidade varchar(100) not null,
+    telefone varchar(20) not null,
+    ativo smallint not null
+
+);
+
+create table medicos(
+
+    id bigserial not null primary key,
+    nome varchar(100) not null,
+    email varchar(100) not null unique,
+    crm varchar(6) not null unique,
+    especialidade varchar(100) not null,
+    logradouro varchar(100) not null,
+    bairro varchar(100) not null,
+    cep varchar(9) not null,
+    complemento varchar(100),
+    numero varchar(20),
+    uf char(2) not null,
+    cidade varchar(100) not null
+
+
+);
